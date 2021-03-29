0390e8d6a488055a219e3cb56459c1602374fbd7
Thilina Fernando <54390998+mtfernando@users.noreply.github.com>
2021-03-29 16:59:30 +1100

    Intent to quiz activity created
    Main Activity now passes user name to quiz activity and displays the name in the welcome text.

--- app/src/main/java/com/example/sit305quizapp/MainActivity.java
+++ app/src/main/java/com/example/sit305quizapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.sit305quizapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,13 +15,41 @@ public class MainActivity extends AppCompatActivity {
     EditText name;
     Button startButton;
     String userName;
+    Integer finalScore=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         name = findViewById(R.id.nameEditText);
         startButton = findViewById(R.id.startButton);
-        userName =  name.getText().toString();
+
+        //Start Quiz Activity on button click
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userName =  name.getText().toString();
+                name.setText(userName);
+                Intent quizIntent = new Intent(MainActivity.this, QuizActivity.class);
+                quizIntent.putExtra("name", userName);
+                startActivityForResult(quizIntent, RESULT_FIRST_USER);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(resultCode){
+            case(RESULT_FIRST_USER):
+            {
+                finalScore = data.getIntExtra("result", 0);//Get quiz score from the quiz activity
+            } break;
+
+            case(2):
+            {
+                userName = data.getStringExtra("name"); //Get User's name from score activity
+            }
+        }
     }
 
     public void showUserName(View view){
--- app/src/main/java/com/example/sit305quizapp/QuizActivity.java
+++ app/src/main/java/com/example/sit305quizapp/QuizActivity.java
@@ -3,12 +3,40 @@ package com.example.sit305quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class QuizActivity extends AppCompatActivity {
 
+    Integer score, currentQuestion = 1;
+    ProgressBar progressBar;
+    TextView welcomeText, progressText, quesitonTitle, questionDescription;
+    Button ans1,ans2,ans3,submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        //Initializing views with local vars
+        progressBar = findViewById(R.id.progressBar);
+        welcomeText = findViewById(R.id.WelcomeText);
+        progressText = findViewById(R.id.PorgressText);
+        quesitonTitle = findViewById(R.id.questionTitle);
+        questionDescription = findViewById(R.id.questionDesc);
+        ans1 = findViewById(R.id.ans1);
+        ans2 = findViewById(R.id.ans2);
+        ans3 = findViewById(R.id.ans3);
+        submit = findViewById(R.id.submitButton);
+
+        //Set name in welcome string
+        String welcomeTextString = "Welcome " + getIntent().getStringExtra("name" + "!");
+        welcomeText.setText(welcomeTextString);
+
+        //Set progress bar and progress text
+        progressBar.setProgress(currentQuestion);
+        progressText.setText(currentQuestion.toString() + "/5");
     }
 }
