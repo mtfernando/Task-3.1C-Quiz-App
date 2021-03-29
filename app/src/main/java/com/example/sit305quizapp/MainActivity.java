e906ddc174297b30d5b86df3aeb86e7c27c5c8d8
Thilina Fernando <54390998+mtfernando@users.noreply.github.com>
2021-03-29 13:37:11 +1100

    MainActivity toasts the user's name

--- app/src/main/java/com/example/sit305quizapp/MainActivity.java
+++ app/src/main/java/com/example/sit305quizapp/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.sit305quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText name;
+    Button startButton;
+    String userName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name = findViewById(R.id.nameEditText);
+        startButton = findViewById(R.id.startButton);
+        userName =  name.getText().toString();
+    }
+
+    public void showUserName(View view){
+        Toast.makeText(getApplicationContext(), "Welcome " + userName, Toast.LENGTH_SHORT).show();
     }
 }
